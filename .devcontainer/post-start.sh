f861a5850d3df4d4a59956daad8a3d1f63c92550
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-10-17 16:55:32 +0200

    update

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,7 +4,3 @@
 for repo in pandoc/latex:3.2.0 lfoppiano/grobid:0.8.1 jbarlow83/ocrmypdf:latest; do
   docker pull $repo;
 done;
-
-# Open README.md in VS Code preview mode
-code README.md --reuse-window --wait
-code -r --command 'markdown.showPreview'
